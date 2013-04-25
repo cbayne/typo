5035e746aa3ed495c90e07665b2f3c3ad44778a1
cbayne <christina.bayne@gmail.com>
2013-04-25 18:23:32 -0400

    article

--- app/models/article.rb
+++ app/models/article.rb
@@ -61,7 +61,28 @@ class Article < Content
 
   setting :password,                   :string, ''
 
-  def initialize(*args)
+   def merge_with(merge_article_id)
+    merge_article = Article.find_by_id(merge_article_id)
+
+    # title and author come for "free"
+    # merge bodies of the articles
+    if !self.body
+      self.body = merge_article.body
+    elsif merge_article.body
+      self.body += merge_article.body
+    end
+
+    # move comments from merge_article to edit_article
+    self.comments << merge_article.comments
+    self.save
+
+    # must "re-get" merge_article with new comments list or else comments will
+    # be deleted due to cascade
+    merge_article = Article.find_by_id(merge_article_id)
+    merge_article.destroy
+  end
+ 
+   def initialize(*args)
     super
     # Yes, this is weird - PDC
     begin
@@ -469,24 +490,5 @@ class Article < Content
   end
 
 
-  def merge_with(merge_article_id)
-    merge_article = Article.find_by_id(merge_article_id)
-
-    # title and author come for "free"
-    # merge bodies of the articles
-    if !self.body
-      self.body = merge_article.body
-    elsif merge_article.body
-      self.body += merge_article.body
-    end
-
-    # move comments from merge_article to edit_article
-    self.comments << merge_article.comments
-    self.save
-
-    # must "re-get" merge_article with new comments list or else comments will
-    # be deleted due to cascade
-    merge_article = Article.find_by_id(merge_article_id)
-    merge_article.destroy
-  end
+ 
 end
